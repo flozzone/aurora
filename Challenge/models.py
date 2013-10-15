5a8c850b68b87e17c42d9c3085279115d47b568a
grisuTplus <woazboat1>
2013-10-15 18:19:30 +0200

    basic model fields

--- Challenge/models.py
+++ Challenge/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.TextField()
+    cardImageUrl = models.CharField('card url', max_length=100)
+    backgroundImageUrl = models.CharField('background url', max_length=100)
--- Elaboration/models.py
+++ Elaboration/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Elaboration(models.Model):
+    challenge = models.ForeignKey('Challenge.Challenge')
+    portfolio = models.ForeignKey('Portfolio.Portfolio')
+    user = models.ForeignKey('PortfolioUser.PortfolioUser')
+    creationDate = models.DateTimeField(auto_now_add=True)
