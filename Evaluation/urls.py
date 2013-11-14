0a371f1105874fc318515ade4df8218363f805eb
grisuTplus <christoph.boerner@testplus.at>
2013-11-14 19:45:09 +0100

    evaluation view;

--- Evaluation/urls.py
+++ Evaluation/urls.py
@@ -4,14 +4,5 @@ from django.http import HttpResponsePermanentRedirect
 
 urlpatterns = patterns('',
     url(r'^evaluation/$', 'Evaluation.views.evaluation'),
-    #url(r'^challenges/stack$', 'Challenge.views.challenges_stack'),
-    #url(r'^challenges/challenge$', 'Challenge.views.challenge_detail'),
-    #url(r'^challenges/submit$', 'Challenge.views.submit_challenge'),
-    #url(r'^challenges/autosave/$', 'Elaboration.views.save_elaboration'),
-    url(r'^autocomplete_challenge/$', 'Evaluation.views.autocomplete_challenge', name='evaluation_autocomplete_challenge'),
-    url(r'^autocomplete_stack/$', 'Evaluation.views.autocomplete_stack', name='evaluation_autocomplete_stack'),
-    url(r'^autocomplete_user/$', 'Evaluation.views.autocomplete_user', name='evaluation_autocomplete_user'),
-    url(r'^search/$', 'Evaluation.views.search', name='evaluation_search'),
-    url(r'^submission/$', 'Evaluation.views.get_submission'),
-    url(r'^submissions/$', 'Evaluation.views.get_submissions'),
+    url(r'^submission/$', 'Evaluation.views.submission'),
 )
--- Evaluation/views.py
+++ Evaluation/views.py
@@ -15,62 +15,11 @@ def evaluation(request):
     return render_to_response('evaluation.html', {'challenges': challenges}, context_instance=RequestContext(request))
 
 @login_required()
-def autocomplete_challenge(request):
-    term = request.GET.get('term', '')
-    challenges = Challenge.objects.all().filter(title__istartswith=term).order_by('title')
-    titles = [challenge.title for challenge in challenges[:20]]
-    json = simplejson.dumps(titles, ensure_ascii=False)
-    return HttpResponse(json, mimetype='application/json; charset=utf-8')
-
-@login_required()
-def autocomplete_stack(request):
-    term = request.GET.get('term', '')
-    stacks = Stack.objects.all().filter(title__istartswith=term).order_by('title')
-    titles = [stack.title for stack in stacks[:20]]
-    json = simplejson.dumps(titles, ensure_ascii=False)
-    return HttpResponse(json, mimetype='application/json; charset=utf-8')
-
-@login_required()
-def autocomplete_user(request):
-    term = request.GET.get('term', '')
-    pusers = PortfolioUser.objects.all().filter(nickname__istartswith=term).order_by('nickname')
-    titles = [puser.nickname for puser in pusers[:20]]
-    json = simplejson.dumps(titles, ensure_ascii=False)
-    return HttpResponse(json, mimetype='application/json; charset=utf-8')
-
-@login_required()
-def search(request):
-    users = stacks = challenges = {}
-    if 'challenge_request' in request.GET:
-        query = request.GET.get('challenge_request', '')
-        challenges = Challenge.objects.all().filter(title__icontains=query).order_by('title')
-    if 'stack_request' in request.GET:
-        query = request.GET.get('stack_request', '')
-        stacks = Stack.objects.all().filter(title__icontains=query).order_by('title')
-    if 'user_request' in request.GET:
-        query = request.GET.get('user_request', '')
-        users = PortfolioUser.objects.all().filter(nickname__icontains=query).order_by('nickname')
-    html = render_to_response('search.html', {'challenges': challenges, 'stacks': stacks, 'users': users})
-    return html
-
-@login_required()
-def get_submission(request):
-    if 'id' in request.GET:
-        challenge_id = request.GET.get('id', '')
-        challenge = Challenge.objects.get(pk=challenge_id)
-        if Elaboration.objects.all().filter(challenge=challenge).exists():
-            elaboration = Elaboration.objects.all().filter(challenge=challenge).order_by('id')[0]  # TODO: submission_time must be != null, check if elaboration exists
-            html = render_to_response('submission.html', {'elaboration': elaboration})
-            return html
-        return HttpResponse("No Submissions found.")
-
-
-@login_required()
-def get_submissions(request):
+def submission(request):
     if 'challenge_id' in request.GET:
         challenge_id = request.GET.get('challenge_id', '')
         challenge = Challenge.objects.get(pk=challenge_id)
 
         elaborations = challenge.get_submissions()
-        html = render_to_response('submission.html', {'elaboration': elaborations[0]})
+        html = render_to_response('submission.html', {'elaborations': elaborations})
         return html
