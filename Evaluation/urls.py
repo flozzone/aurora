263530bfe7f54697679f58b1f5f3c87d4f10b12f
grisuTplus <woazboat1>
2013-11-24 08:45:57 +0100

    removed deprecated ursl;

--- Evaluation/urls.py
+++ Evaluation/urls.py
@@ -4,10 +4,7 @@ from django.http import HttpResponsePermanentRedirect
 
 urlpatterns = patterns('',
     url(r'^evaluation/$', 'Evaluation.views.evaluation'),
-    url(r'^submission/$', 'Evaluation.views.submission'),
-    url(r'^waiting/$', 'Evaluation.views.waiting'),
     url(r'^submit_evaluation/$', 'Evaluation.views.submit_evaluation'),
-
     url(r'^overview/$', 'Evaluation.views.overview'),
     url(r'^update_overview/$', 'Evaluation.views.update_overview'),
     url(r'^detail/$', 'Evaluation.views.detail'),
