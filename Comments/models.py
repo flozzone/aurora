3940e07174034d676440aef9fdfa3c817e1c0d7e
queltos <queltos>
2013-11-25 03:49:27 +0100

    rudimentary implementation of relative posting time added first working test added

--- Comments/models.py
+++ Comments/models.py
@@ -10,8 +10,26 @@ class Comment(models.Model):
 
     @property
     def post_date_relative(self):
-        post_date_rel = timezone.timedelta(self.post_date - timezone.now())
-        return post_date_rel
+        timedelta = timezone.now() - self.post_date
+
+        # TODO beautify with babel: http://stackoverflow.com/questions/410221/natural-relative-days-in-python
+        if timedelta.days > 6:
+            weeks = timedelta.days/7
+            return str(weeks) + " weeks ago" if weeks > 1 else "a week ago"
+
+        days = timedelta.days
+        if days > 0:
+            return str(days) + " days ago" if days > 1 else "a day ago"
+
+        hours = timedelta.seconds/3600
+        if hours > 0:
+            return str(hours) + " hours ago" if hours > 1 else "an hour ago"
+
+        minutes = timedelta.seconds/60
+        if minutes > 0:
+            return str(minutes) + " minutes ago" if minutes > 1 else "a minute ago"
+
+        return "just a moment ago"
 
     def __unicode__(self):
         return self.text[:20]
--- Comments/tests.py
+++ Comments/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from Comments.models import Comment
+from PortfolioUser.models import PortfolioUser
+from django.utils import timezone
 
-# Create your tests here.
+
+def create_comment(text, author, days=0, minutes=0, seconds=0):
+    delta = timezone.timedelta(days=-days, seconds=-(minutes*60) + seconds)
+    post_date = timezone.now() + delta
+    comment = Comment.objects.create(text=text, author=author, post_date=post_date)
+    return comment
+
+
+def dummy_user_generator():
+    i = 0
+    while True:
+        i += 1
+        n = str(i)
+        user = PortfolioUser(username='du'+n, first_name='first'+n,
+                             last_name='last'+n, email='du'+n+'@foo.bar')
+        user.nickname = 'duni' + n
+        user.password = 'dupa' + n
+
+        yield user
+
+
+class CommentMethodTests(TestCase):
+    user_generator = dummy_user_generator()
+
+    def test_post_date_relative_days_ago(self):
+        u = self.user_generator.next()
+        u.save()
+        c = create_comment("helo und so", u, days=2, minutes=8)
+        c.save()
+        self.assertEqual(c.post_date_relative, "2 days ago")
