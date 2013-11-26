cb0e33d19c07126e2e95662bb241734bb6966113
queltos <queltos>
2013-11-26 17:56:33 +0100

    testing overloading context in views

--- Comments/views.py
+++ Comments/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 from django.views.generic import ListView
 from Comments.models import Comment
+from django.contrib.auth.decorators import login_required
 
 
 class CommentList(ListView):
-    model = Comment
+    #model = Comment
+    queryset = Comment.objects.order_by('-post_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentList, self).get_context_data(**kwargs)
+        context['foo'] = 'krawuzikabuzi'
+        return context
