db204249ee07fa1bdbb6167532062b71517f9872
queltos <queltos>
2013-11-27 18:41:37 +0100

    views file formatted

--- Review/views.py
+++ Review/views.py
@@ -7,10 +7,11 @@ from Elaboration.models import Elaboration
 from Challenge.models import Challenge
 from ReviewQuestion.models import ReviewQuestion
 
+
 def create_context_review(request):
     data = {}
     if 'id' in request.GET:
-        user=PortfolioUser.objects.filter(user_ptr=request.user)[0]
+        user = PortfolioUser.objects.filter(user_ptr=request.user)[0]
         challenge = Challenge.objects.get(pk=request.GET.get('id'))
         review = Review.get_open_review(challenge, user)
         if review:
@@ -32,10 +33,12 @@ def create_context_review(request):
 
     return data
 
+
 @login_required()
 def review(request):
     data = create_context_review(request)
     return render_to_response('review.html', data, context_instance=RequestContext(request))
+
 
 @login_required()
 def review_page(request):
