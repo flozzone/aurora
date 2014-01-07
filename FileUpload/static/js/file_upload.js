5f49699b4b718875699e8fc44bda61d649b67474
martflu <martin.flucka@gmail.com>
2014-01-07 21:53:29 +0100

    file upload disabled for submitted elaborations

--- FileUpload/static/js/file_upload.js
+++ FileUpload/static/js/file_upload.js
@@ -1,12 +1,23 @@
 Dropzone.autoDiscover = false;
 
 $(file_upload_loaded);
-
+var dropzone_instance;
 function file_upload_loaded() {
-    Dropzone.options.dropzone = {
+    var is_submitted = $("#dropzone").hasClass('is_submitted');
+
+    var read_write_options = {
         maxFilesize: 100, // MB
         addRemoveLinks: true,
         init: function () {
+            if (is_submitted) {
+                console.log('is submitted');
+                $('.dropzone').removeClass('dz-clickable');
+                $('.dropzone').click(function (e) {
+                    console.log('bla');
+                    e.preventDefault();
+                    return false;
+                });
+            }
             this.on("success", function (file, response) {
                 file.image_url = response;
                 var elaboration_id = $('#elaboration_id').val();
@@ -17,11 +28,24 @@ function file_upload_loaded() {
             });
             this.on("removedfile", function (file) {
                 var url = '/fileupload/remove?url=' + file.image_url;
-                $.get(url, function (data) {});
+                $.get(url, function (data) {
+                });
             });
         }
     };
-    var dropzone = new Dropzone("#dropzone");
+
+    var read_options = {
+        clickable: false,
+        init: function () {
+            $('.dropzone').removeClass('dz-clickable');
+        }
+    };
+    if (is_submitted) {
+        Dropzone.options.dropzone = read_options;
+    } else {
+        Dropzone.options.dropzone = read_write_options;
+    }
+    dropzone_instance = new Dropzone("#dropzone");
     var elaboration_id = $('#elaboration_id').val();
     if (elaboration_id === '') {
         var challenge_id = $('.challenge').attr('id');
@@ -34,7 +58,6 @@ function file_upload_loaded() {
     } else {
         load_files(elaboration_id)
     }
-
 }
 
 function load_files(elaboration_id) {
@@ -44,9 +67,9 @@ function load_files(elaboration_id) {
         data.forEach(function (file) {
             // Create the mock file:
             var mockFile = { name: file.name, size: file.size, image_url: file.path, type: 'image.*', status: Dropzone.success};
-            dropzone.emit("addedfile", mockFile);
-            dropzone.emit("thumbnail", mockFile, '/' + file.path);
-            dropzone.files.push(mockFile);
+            dropzone_instance.emit("addedfile", mockFile);
+            dropzone_instance.emit("thumbnail", mockFile, '/' + file.path);
+            dropzone_instance.files.push(mockFile);
             $(mockFile.previewElement).find('img').wrap(function () {
                 return "<a href='/" + file.path + "' data-lightbox='preview' title='" + file.name + "'></div>";
             });
