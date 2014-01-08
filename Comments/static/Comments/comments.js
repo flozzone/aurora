6f506cb057807053c40b16e257b094510f4074ec
queltos <queltos>
2014-01-08 15:19:07 +0100

    jquery ajax used for commenting now

--- Comments/static/Comments/comments.js
+++ Comments/static/Comments/comments.js
@@ -1,33 +1,78 @@
 /**
  * Created by dan on 12/21/13.
  */
+$(document).ready(function() {
+});
 
-function post_comment() {
-//    var response;
-
+function postComment() {
     $.ajax({
-        url: "post_comment",
-        // the data to send (will be converted to a query string)
-        data: {
-            id: 123
-        },
-        // whether this is a POST or GET request
+        url: "/post_comment/",
+        data: $("#commentForm").serialize(),
         type: "POST",
         // the type of data we expect back
-        dataType: "json",
-        success: function (json) {
-            $("<h1/>").text("Yay").appendTo("body");
-            alert(json);
-//            $("<div class=\"content\"/>").html(json.html).appendTo("body");
-//            $("<div class=\"content\"/>").html(json.html).appendTo("body");
+        dataType: 'html',
+        success: function (response) {
+            $("#commentText").val('');
+
+            $('#comments').prepend(response)
         },
         error: function (xhr, status) {
-            alert("Sorry, there was a problem!");
+//            alert("Sorry, there was a problem!");
         },
         complete: function (xhr, status) {
-            alert("Request completed successfully");
         }
     });
 
 //    console.log(response); // undefined
+}
+
+function formSubmitButton() {
+    $("#commentForm").submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "/post_comment/",
+            data: $(this).serialize(),
+            type: "POST",
+            // the type of data we expect back
+            dataType: "json",
+            success: function (json) {
+                alert($("#commentText").val)
+                $("<h1/>").text("Yay").appendTo("body");
+                alert(json['text']);
+            },
+            error: function (xhr, status) {
+                alert("Sorry, there was a problem!");
+            },
+            complete: function (xhr, status) {
+                alert("Request completed successfully");
+            }
+        });
+        return false;
+    });
+}
+
+function showReplyForm(that) {
+//    look for form
+//    if form doesnt exist: create
+//    if it does exist: unhide
+//    that.append($('<h1/>').text('blub'));
+    alert(that.text);
+}
+
+function pollForChanges() {
+    $.post('ajax/test.html', function (data) {
+        alert(data);  // process results here
+        setTimeout(pollForChanges(), 5000);
+    });
+}
+
+function updateComments() {
+    $.get('comment_update', { last_comment_id: id , reference_object_id: reference_object_id}, function(response){
+//        if we got new stuff, update DOM
+    });
+}
+
+function postReply(id) {
+    alert(id);
 }
--- Comments/views.py
+++ Comments/views.py
@@ -30,31 +30,38 @@ class CommentList(ListView):
 class CommentForm(forms.Form):
     reference_type_id = forms.IntegerField(widget=forms.HiddenInput)
     reference_id = forms.IntegerField(widget=forms.HiddenInput)
-    text = forms.CharField(widget=forms.Textarea, label='')
+    text = forms.CharField(widget=forms.Textarea(attrs={'id': 'commentText'}), label='')
 
 
 @require_POST
 @login_required
 def post_comment(request):
-#    form = CommentForm(request.POST)
-#    if form.is_valid():
-#        user = PortfolioUser.objects.get(id=request.user.id)
-#        ref_type_id = form.cleaned_data['reference_type_id']
-#        ref_obj_id = form.cleaned_data['reference_id']
-#        ref_obj_model = ContentType.objects.get_for_id(ref_type_id).model_class()
-#        ref_obj = ref_obj_model.objects.get(id=ref_obj_id)
-#        comment = Comment.objects.create(text=form.cleaned_data['text'],
-#                                         author=user,
-#                                         content_object=ref_obj,
-#                                         post_date=timezone.now())
-#        comment.save()
-    data = {'text': 'gehabdichwohl'}
-    return HttpResponse(json.dumps(data), mimetype="application/json")
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        user = PortfolioUser.objects.get(id=request.user.id)
+        ref_type_id = form.cleaned_data['reference_type_id']
+        ref_obj_id = form.cleaned_data['reference_id']
+        ref_obj_model = ContentType.objects.get_for_id(ref_type_id).model_class()
+        ref_obj = ref_obj_model.objects.get(id=ref_obj_id)
+        comment = Comment.objects.create(text=form.cleaned_data['text'],
+                                         author=user,
+                                         content_object=ref_obj,
+                                         post_date=timezone.now())
+        comment.save()
+        data = {'author': comment.author.username,
+                # 'post_date': comment.post_date,
+                'text': comment.text}
+    #return HttpResponse(json.dumps(data), content_type="application/json")
+    return render_to_response('Comments/comment.html', {'comment': comment}, context_instance = RequestContext(request))
     #return HttpResponseRedirect(reverse('Comments:feed'))
 
 
 def feed(request):
-    o = CommentReferenceObject.objects.get(id=1)
+    try:
+        o = CommentReferenceObject.objects.get(id=1)
+    except CommentReferenceObject.DoesNotExist:
+        CommentReferenceObject().save()
+        o = CommentReferenceObject.objects.get(id=1)
     return render(request, 'Comments/feed.html', {'object': o})
 
 
