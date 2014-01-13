0be2e91bafda526e139dbccec8e85667b6e37778
martflu <martin.flucka@gmail.com>
2014-01-13 11:32:44 +0100

    profile adde

--- AmanamanProjekt/urls.py
+++ AmanamanProjekt/urls.py
@@ -2,26 +2,25 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'AmanamanProjekt.views.home', name='home'),
-    url(r'^login/$', 'PortfolioUser.views.login'),
-    url(r'^signin/$', 'PortfolioUser.views.signin'),
-    url(r'^signout/$', 'PortfolioUser.views.signout'),
-    url(r'', include('Challenge.urls')),
-    url(r'', include('Elaboration.urls')),
-    url(r'', include('Evaluation.urls')),
-    url(r'', include('Review.urls')),
-    url(r'', include('FileUpload.urls')),
-    url(r'', include('Comments.urls', namespace='Comments')),
+                       # Examples:
+                       url(r'^$', 'AmanamanProjekt.views.home', name='home'),
+                       url(r'', include('PortfolioUser.urls')),
+                       url(r'', include('Challenge.urls')),
+                       url(r'', include('Elaboration.urls')),
+                       url(r'', include('Evaluation.urls')),
+                       url(r'', include('Review.urls')),
+                       url(r'', include('FileUpload.urls')),
+                       url(r'', include('Comments.urls', namespace='Comments')),
 
-    # url(r'^AmanamanProjekt/', include('AmanamanProjekt.foo.urls')),
+                       # url(r'^AmanamanProjekt/', include('AmanamanProjekt.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       url(r'^admin/', include(admin.site.urls)),
 )
---
+++ PortfolioUser/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^login/$', 'PortfolioUser.views.login'),
+                       url(r'^signin/$', 'PortfolioUser.views.signin'),
+                       url(r'^signout/$', 'PortfolioUser.views.signout'),
+                       url(r'^profile/$', 'PortfolioUser.views.profile'),
+                       url(r'^profile/save/$', 'PortfolioUser.views.save'),
+)
+
+
+
