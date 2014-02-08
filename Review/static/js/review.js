27ffdb59a02815c7741b1482a662a3fc818eedbe
martflu <martin.flucka@gmail.com>
2014-02-08 17:50:35 +0100

    fixed review again

--- Review/static/js/review.js
+++ Review/static/js/review.js
@@ -20,13 +20,13 @@ function submit_clicked(event) {
     data['challenge_id'] = challenge_id;
     data['review_id'] = review_id;
     data['answers'] = [];
-    $(".answer").each(function (index) {
-        var answer_object = $(this);
+    $(".question_container").each(function (index) {
+        var answer_object = $(this).find('.answer');
         var answer = null;
         if (answer_object.hasClass('boolean_answer')) {
             answer = answer_object.find('input').first().is(':checked');
         } else {
-            answer = answer_object.find('#text_answer').html();
+            answer = answer_object.find('#text_answer').val();
         }
         var question = answer_object.parent().find('.question').first();
         var question_id = question.attr('id');
@@ -40,6 +40,7 @@ function submit_clicked(event) {
     data['appraisal'] = $('input[name=appraisal]:checked').val();
 
     ajax_setup()
+    console.log(data);
     var args = {
         type: "POST",
         url: "/challenges/challenge_review/review_answer/",
