9a321b72cd7687615caf26cb6c67bcb5344e1a10
peterpur <purg@igw.tuwien.ac.at>
2014-02-24 12:04:33 +0100

    FEED persistent comment filtering w/ javascript, css and cookies, and nothing else. /me likes

--- Comments/static/Comments/feed.js
+++ Comments/static/Comments/feed.js
@@ -10,15 +10,20 @@ $(window).load( function() {
 
 
 function filter(fx) {
-	document.cookie = "filtercookie="+fx;
+	document.cookie = "filtercookie="+fx+"; expires=Tue, 18 Jan 2038 03:14:06 GMT";
 	if (typeof fx == "string") {fx = parseInt(fx);}
+	$('.filterbtn').removeClass('hilited')
+	$('#'+fx).addClass('hilited')
 	switch(fx) {
 	case 1: $('.response_top,.comment_top').next().show().next().show();$('.r_list').show(); break;
-	case 2: $('.response:not(.staff_author)').children('.response_top').next().hide().next().hide();
+	case 2: $('.response_top,.comment_top').next().show().next().show();$('.r_list').show(); 
+		 	$('.response:not(.staff_author)').children('.response_top').next().hide().next().hide();
 			$('.comment:not(.staff_author)').children('.comment_body').hide().next().hide();
 			$('#'+$('.response.staff_author').parent().attr('class').split(' ')[0].slice(2)).children('.comment_body').show().next().show(); break;
-	case -1:$('.neg0').next().hide().next().hide(); break;
-	case -5:$('.neg5').next().hide().next().hide(); break;
+	case -1:$('.response_top,.comment_top').next().show().next().show();$('.r_list').show();
+			$('.neg0').next().hide().next().hide(); break;
+	case -5:$('.response_top,.comment_top').next().show().next().show();$('.r_list').show();
+			$('.neg5').next().hide().next().hide(); break;
 	case 0: $('.response_top,.comment_top').next().hide().next().hide();$('.r_list').hide(); break;
 	}
 }
