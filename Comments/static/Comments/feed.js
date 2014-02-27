fade92f37c5282f212ec153b21501cd97ffd59ae
peterpur <purg@igw.tuwien.ac.at>
2014-02-27 09:12:34 +0100

    POINTS points to aurora image position now works across the app, thanks to a cookie

--- Comments/static/Comments/feed.js
+++ Comments/static/Comments/feed.js
@@ -31,18 +31,6 @@ function filter(fx,usr) {
 	}
 }
 
-function getCookie(cname) {
-var name = cname + "=";
-var ca = document.cookie.split(';');
-for(var i=0; i<ca.length; i++) 
-  {
-  var c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-  }
-return "";
-}
-
-
 function headClick(aDiv) {
 	$('.filterbtn').removeClass('hilited');
 	$(aDiv).toggleClass('hided');
