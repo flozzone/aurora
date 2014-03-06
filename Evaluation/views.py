de3b588f2776ef9c29d4c38025668ebbc9de8e5f
grisuTplus <christoph.boerner@testplus.at>
2014-03-06 19:05:19 +0100

    added staff_member_required decorator in evaluation;

--- Evaluation/views.py
+++ Evaluation/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import CharField
@@ -27,6 +28,7 @@ from Notification.models import Notification
 
 
 @login_required()
+@staff_member_required
 def evaluation(request):
     # TODO: delete this snippet, fetches gravatar images for every user only for test cases.
     for puser in PortfolioUser.objects.all():
@@ -57,6 +59,7 @@ def evaluation(request):
 
 
 @login_required()
+@staff_member_required
 def overview(request):
     challenges = Challenge.objects.all()
     missing_reviews = Elaboration.get_missing_reviews()
@@ -68,6 +71,7 @@ def overview(request):
 
 
 @login_required()
+@staff_member_required
 def update_overview(request):
     if request.GET.get('data', '') == "missing_reviews":
         print("loading missing reviews...")
@@ -100,6 +104,7 @@ def update_overview(request):
 
 
 @login_required()
+@staff_member_required
 def questions(request):
     print("loading questions...")
     challenges = Challenge.get_questions(RequestContext(request))
@@ -113,6 +118,7 @@ def questions(request):
 
 
 @login_required()
+@staff_member_required
 def detail(request):
     # get selected elaborations from session
     elaborations = []
@@ -195,6 +201,7 @@ def detail(request):
 
 
 @login_required()
+@staff_member_required
 def stack(request):
     elaboration = Elaboration.objects.get(pk=request.session.get('elaboration_id', ''))
     stack_elaborations = elaboration.user.get_stack_elaborations(elaboration.challenge.get_stack())
@@ -203,6 +210,7 @@ def stack(request):
 
 
 @login_required()
+@staff_member_required
 def others(request):
     # get selected elaborations from session
     elaboration = Elaboration.objects.get(pk=request.session.get('elaboration_id', ''))
@@ -229,12 +237,14 @@ def others(request):
 
 
 @login_required()
+@staff_member_required
 def challenge_txt(request):
     elaboration = Elaboration.objects.get(pk=request.session.get('elaboration_id', ''))
     return render_to_response('challenge_txt.html', {'challenge': elaboration.challenge}, RequestContext(request))
 
 
 @csrf_exempt
+@staff_member_required
 def save_evaluation(request):
     elaboration_id = request.POST['elaboration_id']
     evaluation_text = request.POST['evaluation_text']
@@ -253,6 +263,7 @@ def save_evaluation(request):
 
 
 @csrf_exempt
+@staff_member_required
 def submit_evaluation(request):
     elaboration_id = request.POST['elaboration_id']
     evaluation_text = request.POST['evaluation_text']
@@ -285,6 +296,7 @@ def submit_evaluation(request):
 
 
 @csrf_exempt
+@staff_member_required
 def reopen_evaluation(request):
     elaboration_id = request.POST['elaboration_id']
     elaboration = Elaboration.objects.get(pk=elaboration_id)
@@ -308,6 +320,7 @@ def reopen_evaluation(request):
 
 
 @csrf_exempt
+@staff_member_required
 def set_appraisal(request):
     review_id = request.POST['review_id']
     appraisal = request.POST['appraisal']
@@ -321,6 +334,7 @@ def set_appraisal(request):
 
 @csrf_exempt
 @login_required()
+@staff_member_required
 def select_challenge(request):
     selected_challenge = request.POST['selected_challenge']
 
@@ -339,6 +353,7 @@ def select_challenge(request):
 
 @csrf_exempt
 @login_required()
+@staff_member_required
 def search(request):
     search_user = request.POST['search_user']
     search_all = request.POST['search_all']
@@ -411,6 +426,7 @@ def search(request):
 
 
 @login_required()
+@staff_member_required
 def autocomplete_challenge(request):
     term = request.GET.get('term', '')
     challenges = Challenge.objects.all().filter(title__istartswith=term)
@@ -420,6 +436,7 @@ def autocomplete_challenge(request):
 
 
 @login_required()
+@staff_member_required
 def autocomplete_user(request):
     term = request.GET.get('term', '')
     studies = PortfolioUser.objects.all().filter(
@@ -430,6 +447,7 @@ def autocomplete_user(request):
 
 
 @login_required()
+@staff_member_required
 def load_reviews(request):
     if not 'elaboration_id' in request.GET:
         return False;
@@ -443,6 +461,7 @@ def load_reviews(request):
 
 @csrf_exempt
 @login_required()
+@staff_member_required
 def review_answer(request):
     if request.POST:
         data = request.body.decode(encoding='UTF-8')
@@ -467,6 +486,7 @@ def review_answer(request):
 
 
 @login_required()
+@staff_member_required
 def back(request):
     selection = request.session.get('selection', 'error')
     if selection == "missing_reviews":
