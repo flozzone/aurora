bc2c840f70e6c95e6cb326dbd8c301b2e738a863
queltos <queltos@gmail.com>
2014-03-10 15:10:24 +0100

    cleanup and gravatar fetching for sso added

--- PortfolioUser/views.py
+++ PortfolioUser/views.py
@@ -29,7 +29,6 @@ def signin(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
-    # user = authenticate(params='blablu')
 
     if user is None:
         response_data = {'success': False, 'message': 'Your username or password was incorrect.'}
@@ -47,21 +46,6 @@ def signin(request):
 
     response_data = {'success': True}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
-
-
-# TODO delete
-def dat_secret_test(request):
-    mn = '0302857'
-    fname = 'Daniel'
-    lname = 'Domberger'
-
-    try:
-        user = PortfolioUser.objects.get(matriculation_number=mn, first_name=fname, last_name=lname)
-    except PortfolioUser.DoesNotExist:
-        user = PortfolioUser.objects.create(username=mn, matriculation_number=mn, first_name=fname, last_name=lname)
-
-    user = authenticate(username=mn)
-    django_login(request, user)
 
 
 def signout(request):
@@ -95,6 +79,9 @@ def sso_auth_callback(request):
         return render_to_response('login.html', {'error_message': 'Your user has been deactivated'}, context_instance=RequestContext(request))
 
     django_login(request, user)
+
+    if not user.avatar:
+        user.get_gravatar()
 
     return redirect('/')
 
