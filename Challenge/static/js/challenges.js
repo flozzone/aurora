7651e351fde798a17d832c4788b7350f990cb245
peterpur <purg@igw.tuwien.ac.at>
2014-03-14 09:04:51 +0100

    FAVICON done

--- Challenge/static/js/challenges.js
+++ Challenge/static/js/challenges.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('.course_selected').removeClass('irrelevant');
 	$('#challenges-li').addClass('uRhere');
+	window.document.title="Aurora: Challenges"
 });
 
 
--- Notification/static/js/notification.js
+++ Notification/static/js/notification.js
@@ -2,6 +2,7 @@ function notifications_loaded() {
     $('#read_all_button').click(read_all_button_clicked);
 	$('.course_selected').addClass('irrelevant');
 	$('#notifications-li').addClass('uRhere');
+	window.document.title="Aurora: Notifications"
 }
 
 $(notifications_loaded);
