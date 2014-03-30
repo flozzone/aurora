d2b7118eee2ff3515d27c95e18804b4ee56ddb23
queltos <queltos@gmail.com>
2014-03-30 18:35:19 +0200

    postgres installation added to vagrant provisioning

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -1,2 +1,14 @@
 > database.db && echo no | python manage.py syncdb && python manage.py populate_demo_data
 python manage.py collectstatic
+
+
+### for Postgres ###
+
+#rsync dude@unchained:/home/dude/aurora_backup_latest* ~
+
+#cd
+#gunzip aurora_backup_latest.db.psql.gz
+#sed -i 's/_postgres/postgres/g' aurora_backup_*.db.psql
+#sudo -u postgres sh -c "cat aurora_backup_*.db.psql | psql aurora"
+
+#tar -xzf aurora_backup_latest.files.tar.gz
