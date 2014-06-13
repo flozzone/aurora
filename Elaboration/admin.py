112d16887f650a0a841d5832ec5bc16d1fb725da
peterpur <purg@igw.tuwien.ac.at>
2014-06-13 11:51:29 +0200

    ADMIN added more search capabilities to elaboration table

--- Elaboration/admin.py
+++ Elaboration/admin.py
@@ -16,7 +16,7 @@ class ElaborationAdmin(admin.ModelAdmin):
         ),
     ]
     list_display = ('id', 'challenge', 'user', 'creation_time', 'elaboration_text', 'submission_time', )
-    search_fields = ('user__username',)
+    search_fields = ('user__username','challenge__id',)
     readonly_fields = ("creation_time",)
 
 admin.site.register(Elaboration, ElaborationAdmin)
