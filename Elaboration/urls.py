fe0ad80be068f558d53007ef7939fadba5faa528
grisuTplus <christoph.boerner@testplus.at>
2014-06-30 22:50:57 +0200

    commented start and submit challenge cause end of sem reached;

--- Elaboration/urls.py
+++ Elaboration/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^challenges/submit$', 'Elaboration.views.submit_elaboration'),
+    # url(r'^challenges/submit$', 'Elaboration.views.submit_elaboration'),
     url(r'^challenges/autosave/$', 'Elaboration.views.save_elaboration'),
     url(r'^elaboration/create$', 'Elaboration.views.create_elaboration'),
 )
