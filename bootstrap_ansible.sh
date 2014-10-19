e7f949cd9f83866a7bc8460a535596464cf816e5
queltos <queltos@gmail.com>
2014-10-19 23:39:13 +0200

    changed provisioning - ansible is now only installed when not present - py3env is installed for user vagrant - ipython added again for manage.py shell

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -3,10 +3,16 @@
 # Bootstrap the vagrant VM by installing Ansible and
 # letting ansible do the provisioning in local connection mode
 #
-sudo add-apt-repository -y ppa:rquillo/ansible
-sudo apt-get update
-sudo apt-get install -y python-software-properties
-sudo apt-get install -y ansible
+version=$(ansible version 2>&1)
+
+if echo "$version" | grep -q -i 'command not found'
+then
+  echo "[Vagrant] Ansible not found, installing.."
+
+  add-apt-repository -y ppa:rquillo/ansible 2>&1 >> /home/vagrant/provision.log
+  apt-get update
+  apt-get install -y ansible
+fi
 
 echo 'localhost              ansible_connection=local' > /home/vagrant/hosts
 PYTHONUNBUFFERED=1 ansible-playbook /vagrant/provision_vagrant_vm.yml -i /home/vagrant/hosts
