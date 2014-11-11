851b9f20a6e4c9b2572049972b9491c2965ae480
queltos <queltos@gmail.com>
2014-11-11 02:47:59 +0100

    some more urls beautification
    specifically: using nested includes to group lva pattern suburls and using the view function objects instead of strings describing the view functions

--- AuroraUser/urls.py
+++ AuroraUser/urls.py
@@ -2,16 +2,17 @@ from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from AuroraUser import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        # namespace comes from main urls.py
-                       url(r'login/$', 'AuroraUser.views.login', name='login'),
-                       url(r'signin/$', 'AuroraUser.views.signin', name='signin'),
-                       url(r'signout/$', 'AuroraUser.views.signout', name='signout'),
+                       url(r'^login/$', views.login, name='login'),
+                       url(r'^signin/$', views.signin, name='signin'),
+                       url(r'^signout/$', views.signout, name='signout'),
                        url(r'^course/$', 'AuroraUser.views.course'),
                        url(r'^profile/$', 'AuroraUser.views.profile'),
                        url(r'^profile/save/$', 'AuroraUser.views.profile_save'),
                        url(r'^sso_auth_callback$', 'AuroraUser.views.sso_auth_callback'),
-)
+                       )
