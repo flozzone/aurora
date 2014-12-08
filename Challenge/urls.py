f75d3b660270df5bec3ad5ef6f845df29e9cb94b
Martin Flucka <martin.flucka@gmail.com>
2014-12-08 17:32:27 +0100

    Change order of urls so default is last

--- Challenge/urls.py
+++ Challenge/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 import Challenge.views
 
 urlpatterns = patterns('',
-                       url(r'$', Challenge.views.challenges, name='home'),
                        url(r'stack$', Challenge.views.stack, name='stack'),
                        url(r'challenge$', Challenge.views.challenge),
+                       url(r'$', Challenge.views.challenges, name='home'),
                        )
