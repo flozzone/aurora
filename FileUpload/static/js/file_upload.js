0dea6d7bbaf0a3aebc58fcdfd20e24456cce242e
Martin Flucka <martin.flucka@gmail.com>
2015-02-19 01:34:50 +0100

    chore: remove code that can not be reached

--- FileUpload/static/js/file_upload.js
+++ FileUpload/static/js/file_upload.js
@@ -78,17 +78,7 @@ function file_upload_loaded() {
     }
     dropzone_instance = new Dropzone("#dropzone");
     var elaboration_id = $('#elaboration_id').val();
-    if (elaboration_id === '') {
-        var challenge_id = $('.challenge').attr('id');
-        var url = SAVE_URL;
-        $.post(url, {'challenge_id': challenge_id, 'elaboration_text': ''}, function (data) {
-            elaboration_id = data;
-            $('#elaboration_id').val(elaboration_id);
-            load_files(elaboration_id, is_submitted)
-        });
-    } else {
-        load_files(elaboration_id, is_submitted)
-    }
+    load_files(elaboration_id, is_submitted)
 }
 
 function load_files(elaboration_id, is_submitted) {
