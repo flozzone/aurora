698ed028150e3c2f4b04d43b37311dca03c69d25
Martin Flucka <martin.flucka@gmail.com>
2015-07-15 12:49:07 +0200

    chore: adapt review answer fix script

--- Review/management/commands/review_answer_fix.py
+++ Review/management/commands/review_answer_fix.py
@@ -1,27 +1,31 @@
 from Review.models import Review
 from ReviewQuestion.models import ReviewQuestion
 from ReviewAnswer.models import ReviewAnswer
+from django.core.management.base import NoArgsCommand
 
-reviews = Review.objects.filter(submission_time__isnull=False)
 
-problems = []
-for review in reviews:
-  for review_question in ReviewQuestion.objects.filter(challenge=review.elaboration.challenge):
-    answers = ReviewAnswer.objects.filter(review=review, review_question=review_question)
-    if len(answers) is 0:
-      problems.append(review)
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
 
-print(len(problems))
+        reviews = Review.objects.filter(submission_time__isnull=False)
 
-script_review_text = "[diese review-antwort ging leider verloren, und wurde von einem script durch diesen text ersetzt]"
+        problems = []
+        for review in reviews:
+            for review_question in ReviewQuestion.objects.filter(challenge=review.elaboration.challenge):
+                answers = ReviewAnswer.objects.filter(review=review, review_question=review_question)
+                if len(answers) is 0:
+                    problems.append(review)
 
-count = 0
-for review in problems:
-  for review_question in ReviewQuestion.objects.filter(challenge=review.elaboration.challenge):
-    answers = ReviewAnswer.objects.filter(review=review, review_question=review_question)
-    if len(answers) is 0:
-      ReviewAnswer(review=review, review_question=review_question, text= script_review_text).save()
-      count += 1
 
-print(count)
-print('All done :D')
+        script_review_text = "[diese review-antwort ging leider verloren, und wurde von einem script durch diesen text ersetzt]"
+
+        count = 0
+        for review in problems:
+            for review_question in ReviewQuestion.objects.filter(challenge=review.elaboration.challenge):
+                answers = ReviewAnswer.objects.filter(review=review, review_question=review_question)
+                if len(answers) is 0:
+                    ReviewAnswer(review=review, review_question=review_question, text= script_review_text).save()
+                    count += 1
+
+        print(count)
+        print('All done :D')
