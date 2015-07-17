6cdfe845bd4a2312554b9a99f9dc2afd76102e4e
Martin Flucka <martin.flucka@gmail.com>
2015-07-18 00:20:06 +0200

    fix: add new endterm_eval_users logic

--- AuroraUser/management/commands/endterm_eval_users.py
+++ AuroraUser/management/commands/endterm_eval_users.py
@@ -1,15 +1,40 @@
-from django.core.management.base import NoArgsCommand
-from AuroraUser.models import  AuroraUser
+from Course.models import Course, CourseUserRelation
+from Review.models import ReviewEvaluation
+from django.core.management.base import BaseCommand
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
-        for user in AuroraUser.objects.filter(is_staff=False):
-            s = "\t".join(["{}"] * 7).format(user.matriculation_number, \
-                                             user.nickname, \
-                                             user.first_name, \
-                                             user.last_name, \
-                                             user.study_code, \
-                                             str(user.last_activity), \
-                                             user.statement)
+
+class Command(BaseCommand):
+
+    help = 'Prints review data for given course short title e.g.: python manage.py result_reviews gsi'
+
+    def handle(self, *args, **options):
+        """
+        user_MNr user_nickname user_vorname user_nachname user_studienkennzahl userLastActivity_date ReviewEvaluationsDone_percent user_statement TAGS_liste
+            wobei TAGS_liste = tag1|tag2|tag3|tag4|tag5... (also tag1+’|’+tag2+’|’+usw)
+        """
+        course_short_title = args[0]
+        course = Course.get_or_raise_404(course_short_title)
+
+        for relation in CourseUserRelation.objects.filter(user__is_staff=False, course=course):
+            user = relation.user
+            tags = []
+            for tag in user.tags.all():
+                tags.append(str(tag))
+
+
+            tags_text = '|'.join(tags)
+            if tags_text == '':
+                tags_text = None
+            s = "\t".join(["{}"] * 9).format(
+                user.matriculation_number,
+                user.nickname,
+                user.first_name,
+                user.last_name,
+                user.study_code,
+                str(user.last_activity),
+                '{0:.2f}'.format(ReviewEvaluation.get_review_evaluation_percent(user, course)),
+                user.statement,
+                tags_text
+            )
 
             print(s)
