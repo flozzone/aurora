a90a16743faa09ef8df9a2519833916fabc93aaa
Martin Flucka <martin.flucka@gmail.com>
2015-07-18 00:34:00 +0200

    feat: update endterm_eval_elabs_final

--- Elaboration/management/commands/endterm_eval_elabs_final.py
+++ Elaboration/management/commands/endterm_eval_elabs_final.py
@@ -1,31 +1,39 @@
-__author__ = 'queltos'
-
-from django.core.management.base import NoArgsCommand
+from Course.models import Course
 from Evaluation.models import Evaluation
+from django.core.management.base import BaseCommand
 
-class Command(NoArgsCommand):
-    def handle_noargs(self, **options):
+
+class Command(BaseCommand):
+
+    help = 'Prints final task elaboration data for given course short title e.g.: python manage.py endterm_eval_elabs_final gsi'
+
+    def handle(self, *args, **options):
         """
-        username(mnr) TAB elabID TAB challenge-title TAB challenge-ID TAB creation time TAB submission time TAB
-        evaluationID TAB tutor TAB evaluation-creationdate TAB evaluation-submissiontime TAB evaluation-points
+        Author_MNr Elab_ID Challenge_ID creation_time submission_time Evaluation_ID EvalTutor_ID EvalCreation_date EvalSubmission_date Eval_points ElabComments_count
         """
+        try:
+            course_short_title = args[0]
+        except IndexError:
+            print(self.help)
+            exit(1)
 
-        evals = Evaluation.objects.all().prefetch_related()
+        course = Course.get_or_raise_404(course_short_title)
 
+        evals = Evaluation.objects.filter(submission__challenge__course=course).prefetch_related()
         for eval in evals:
             elab = eval.submission
             s = "\t".join(["{}"] * 11).format(
-                elab.user.username + " (" + str(elab.user.matriculation_number) + ")",
-                str(elab.id),
-                elab.challenge.title,
-                str(elab.challenge.id),
-                str(elab.creation_time),
-                str(elab.submission_time),
+                elab.user.matriculation_number,
+                elab.id,
+                elab.challenge.id,
+                elab.creation_time,
+                elab.submission_time,
                 eval.id,
-                eval.tutor.display_name,
-                str(eval.creation_date),
-                str(eval.submission_time),
-                str(eval.evaluation_points)
+                eval.tutor.id,
+                eval.creation_date,
+                eval.submission_time,
+                eval.evaluation_points,
+                eval.submission.get_visible_comments_count()
             )
 
             print(s)
